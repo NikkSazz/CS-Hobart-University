00bd15ec732a0920d7970815df159110ee0d115d
Nikolai Sazonov <113256059+NikkSazz@users.noreply.github.com>
2025-02-09 11:39:37 -0500

    Update ooflip.java

--- Intermediate Programming/flip/ooflip.java
+++ Intermediate Programming/flip/ooflip.java
@@ -1,6 +1,40 @@
 package cs225;
+import java.util.Scanner;
+
+/*
+ * @author Nikolai Sazonov
+ */
+
 class ooflip {
+    private static final Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello from OOFlip!");
+        System.out.println("\nWelcome from OOFlip!");
+
+        player[] players = new player[2];
+
+
+        System.out.println("Please Enter Player One's Name: ");
+		players[0] = new player(s.nextLine());
+
+		System.out.println("\nPlease Enter Player Two's Name: ");
+		players[1] = new player(s.nextLine());
+        
+        System.out.println(players[0].name());
+        System.out.println(players[1].name());
+    }
+}
+
+class player {
+    String name;
+    int[] hand;
+    int points;
+
+    public player(String n){
+        name = n;
+        points = 0;
+    }
+
+    public String name() {
+        return name;
     }
 }
