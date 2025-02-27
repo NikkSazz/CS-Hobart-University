ce32702c1216c53b4c1d452320b254c4ac11f9f9
Nikolai Sazonov <113256059+NikkSazz@users.noreply.github.com>
2025-02-27 13:13:38 -0500

    Add files via upload

---
+++ Intermediate Programming/omino/Game.java
@@ -0,0 +1,25 @@
+/**
+ * Game logic for Omino!.
+ * 
+ * @author your name here
+ */
+public class Game extends OminoSubject {
+
+	/**
+	 * Create a new Omino! game. The game must be started in order to play.
+	 */
+	public Game () {
+		// TODO initialize the instance variables
+	}
+
+	public int getBoardWidth () {
+		// TODO return the board width
+		return -1;
+	}
+
+	public int getBoardHeight () {
+		// TODO return the board height
+		return -1;
+	}
+
+}
---
+++ Intermediate Programming/omino/OminoTester.java
@@ -0,0 +1,25 @@
+
+/**
+ * Tester for Omino methods.
+ * 
+ * @author your name here
+ */
+public class OminoTester {
+
+	public static void main ( String[] args ) {
+
+		// the following is the "official" way to programmatically determine whether
+		// or not assertion checking is turned on
+		// it is included here because it is easy to accidentally omit setting the
+		// proper switch in the run configuration and thus not realize that
+		// assertions aren't being checked
+		boolean assertsEnabled = false;
+		assert assertsEnabled = true; // Intentional side-effect!!!
+		System.out.println("assertions are on: " + assertsEnabled);
+		System.out.println();
+
+		// -- add test cases below --------------------------------------------
+		
+	}
+
+}
