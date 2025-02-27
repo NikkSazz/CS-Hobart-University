b577f50f2d22e5e55fdb3f40d4301712d0ca6e2d
Nikolai Sazonov <113256059+NikkSazz@users.noreply.github.com>
2025-02-27 13:57:57 -0500

    Update Block.java

--- Intermediate Programming/omino/Block.java
+++ Intermediate Programming/omino/Block.java
@@ -4,4 +4,19 @@
  */
 public class Block {
 
+	int row;
+	int column;
+	
+	public Block(int r, int c) {
+		this.row = r;
+		this.column = c;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
 }
