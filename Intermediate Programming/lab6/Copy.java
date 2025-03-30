6521406b1dc3b0027142c7044dff1d62926b95f0
Nikolai Sazonov <113256059+NikkSazz@users.noreply.github.com>
2025-03-30 06:10:36 -0400

    copy

---
+++ Intermediate Programming/lab6/Copy.java
@@ -0,0 +1,17 @@
+package lab6;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.Console;
+
+public class Copy {
+
+	static void copy(BufferedReader reader, BufferedWriter writer) {
+		System.out.println("copy()");
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
