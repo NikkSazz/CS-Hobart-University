876130f51af4aed9b64ad0928425ceae0a7709a2
Nikolai Sazonov <113256059+NikkSazz@users.noreply.github.com>
2025-03-30 06:24:21 -0400

    Streams, Output, BufferedReaders

--- Intermediate Programming/lab6/Copy.java
+++ Intermediate Programming/lab6/Copy.java
@@ -1,16 +1,50 @@
 package lab6;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.Console;
+import java.io.*;
+import java.net.URL;
 
 public class Copy {
 
-	static void copy(BufferedReader reader, BufferedWriter writer) {
-		System.out.println("copy()");
+	static void copy(InputStream input, OutputStream output) throws IOException {
+		int read;
+        while ((read = input.read()) != -1) {
+            output.write(read);
+        }
 	}
 	
 	public static void main(String[] args) {
+		if (args.length < 2) {
+			return;
+		}
+		
+		String source = args[0];
+		String destination = args[1];
+		
+		try {
+			
+			InputStream inpStream;
+			if ("-".equals(source)) {
+				inpStream = System.in;
+	        } 
+			else if (source.startsWith("http")) {
+	            URL url = new URL(source);
+	            inpStream = url.openStream();
+	        } else {
+	        	inpStream = new FileInputStream(source);
+	        }
+			
+			
+			OutputStream outStream;
+			if ("-".equals(destination)) {
+	            outStream = System.out;
+	        } 
+			else {
+	            outStream = new FileOutputStream(destination);
+	        }
+			
+			copy(inpStream, outStream);
+			
+		} catch (IOException e) {	}
 		
 	}
 
