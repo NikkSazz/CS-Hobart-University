1521c4af8d3c440ce11ff6608e4ced1156e1f649
Nikolai Sazonov <nsazonov9@gmail.com>
2025-04-26 12:40:56 -0400

    Testing run

--- Intermediate Programming/Adventure/src/Command.java
+++ Intermediate Programming/Adventure/src/Command.java
@@ -4,6 +4,15 @@ public enum Command {
     DROP,
     LOOK,
     INVENTORY,
+    HELP,
     SCORE,
     QUIT;
+
+    static void printCommands() {
+        System.out.println("\n");
+        for(Command c : Command.values()) {
+            System.out.println(c.name());
+        }
+        System.out.println("\n");
+    }
 }
--- Intermediate Programming/Adventure/src/Main.java
+++ Intermediate Programming/Adventure/src/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -6,7 +9,38 @@ public class Main {
     }
 
     public static void play() {
-        
+
+        // TO DO: Keybind ShortCut creation
+
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+
+        Command.printCommands();
+
+        while (true) { 
+            String userInput = scanner.nextLine().toUpperCase();
+
+            Command command = Command.valueOf(userInput);
+
+            switch (command) {
+                case GO -> System.out.println("Go Command.");
+                case MOVE -> System.out.println("Move.");
+                case TAKE -> System.out.println("Take.");
+                case GET -> System.out.println("Get command.");
+                case DROP -> System.out.println("You drop an item from your inventory.");
+                case LOOK -> System.out.println("you looked  around.");
+                case INVENTORY -> System.out.println("You check your inventory.");
+                case HELP -> System.out.println("You asked for help");
+                case SCORE -> System.out.println("Your current score is 69.");
+                case QUIT -> {
+                    System.out.println("Goodbye! Thanks for playing.");
+                    scanner.close();
+                    return;
+                }
+                default -> System.out.println("Unknown command.");
+            }
+
+        }
     }
     
 }
